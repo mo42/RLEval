b54e4ff917e336283b8e031ab32c6dd25ef9afff
Moritz Winter <me@mo42.de>
2017-12-21 10:27:55 +0100

    Add car world test

--- CarWorldTest.cpp
+++ CarWorldTest.cpp
@@ -1,9 +1,18 @@
 #include <CarWorld.h>
+#include <iostream>
 
 using namespace RL;
 
-
 int main(int argc, char* argv[]) {
   CarWorld world;
+  CarParameter state;
+  CarParameter nextState;
+  world.initialState(state);
+  CarAction action;
+  for (int i = 0; i < 256; ++i) {
+    world.act(state, action, nextState);
+    state = nextState;
+    std::cout << state << std::endl;
+  }
   return 0;
 }
